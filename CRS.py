7ff960166eff8540d90a71a116aa82d0cc36bf8c
Anshikay17 <135619357+Anshikay17@users.noreply.github.com>
2024-07-18 22:12:56 +0530

    Update CRS.py

--- CRS.py
+++ CRS.py
@@ -10,7 +10,8 @@ import pandas as pd
 import numpy as np
 import os
 import pickle
-import sklearn
+from sklearn import preprocessing, metrics, model_selection
+
 # Set page configuration with Unicode emoji for the icon
 st.set_page_config(
     page_title="CropWise",
