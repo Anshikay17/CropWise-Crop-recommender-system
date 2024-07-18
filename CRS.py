3434b9d080df6d196a81dff27b0b54369bd17dd0
Anshikay17 <135619357+Anshikay17@users.noreply.github.com>
2024-07-18 22:06:31 +0530

    Update CRS.py

--- CRS.py
+++ CRS.py
@@ -30,7 +30,7 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-crop_model = pickle.load(open('Crop_model (1).sav','rb'))
+crop_model = pickle.load(open('Crop_model (1).sav'))
 
 
 def main():
