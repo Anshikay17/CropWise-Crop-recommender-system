f64a15da0a93bcc83ac230870d762e5081899399
Anshikay17 <135619357+Anshikay17@users.noreply.github.com>
2024-07-23 10:57:25 +0530

    Update CRS.py

--- CRS.py
+++ CRS.py
@@ -43,7 +43,7 @@ st.markdown(
 # Load model with exception handling
 try:
     st.write("Loading model...")
-    crop_model = joblib.load('C:/Users/Dell/Desktop/Crop_recommender_project/RandomForest.pkl')
+    crop_model = joblib.load('RandomForest.pkl')
     st.write("Model loaded successfully.")
 except FileNotFoundError:
     st.error("Model file not found. Please check the file path.")
